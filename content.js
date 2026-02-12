47fbe636f7d8b7ffd08d0cd44f8814978e396fd2
olli becker <ollibecker@olli-MacBook-Pro.local>
2026-02-12 18:10:23 +0200

    working on a script which chagens a youtube thumail to another picture

--- content.js
+++ content.js
@@ -1,1 +1,14 @@
-z
+function checkifyoutube() {
+    const currentUrl = window.location.href;
+    if (currentUrl == "youtube.com") {
+    
+    }
+
+    if (logo) {
+        logo.style.display = 'none';
+        const logo = document.querySelector('yt-icon.ytd-logo');
+        console.log('YouTube logo hidden by extension!');
+    }
+}
+
+checkifyoutube()
